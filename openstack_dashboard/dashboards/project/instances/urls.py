533c7636ae8fb3285cdf0b4e6322b36e6dfeb068
Brian Tully <brian.tully@gmail.com>
2015-04-23 14:40:36 -0400

    update template view so we can show title (hack)
    previously page title was not showing. this is a workaround to get "Instances" page title to show above table

--- openstack_dashboard/dashboards/project/instances/urls.py
+++ openstack_dashboard/dashboards/project/instances/urls.py
@@ -18,7 +18,6 @@
 
 from django.conf.urls import patterns
 from django.conf.urls import url
-from django.views.generic import TemplateView as template
 
 from openstack_dashboard.dashboards.project.instances import views
 
@@ -31,7 +30,7 @@ VIEW_MOD = 'openstack_dashboard.dashboards.project.instances.views'
 urlpatterns = patterns(
     VIEW_MOD,
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^list$', template.as_view(template_name="project/instances/list.html")),
+    url(r'^list$', views.ListView.as_view(), name='list'),
     url(r'^launch$', views.LaunchInstanceView.as_view(), name='launch'),
     url(r'^(?P<instance_id>[^/]+)/$',
         views.DetailView.as_view(), name='detail'),
